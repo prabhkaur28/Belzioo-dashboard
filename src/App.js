2090760b3f54c0d6e230ac97717a5829985a7bc3
prabhjot <prabhjot.synswift@gmail.com>
2024-05-22 17:23:16 +0530

    SYNAWIFT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainRoute from './pages/route/mainroute/MainRoute';
-import Trackorder from './pages/screen/trackorder/Trackorder';
-import Mainpage from './pages/screen/mainpage/Mainpage';
-import AuthRoute from './pages/route/authroute/AuthRoute';
+import MainRoute from './screens/routes/mainroute/MainRoute';
+import Trackorder from './screens/pages/main/trackorder/Trackorder';
+import AuthRoute from './screens/routes/authroute/AuthRoute';
 // import EditProfile from './pages/screen/auth/components/Editprofile/EditProfile';
-import Login from './pages/screen/auth/components/login/Login';
-import Register from './pages/screen/auth/components/register/Register';
+import Login from './screens/pages/auth/components/login/Login';
+import Register from './screens/pages/auth/components/register/Register';
+import Bestoffer from './screens/pages/main/bestoffer/Bestoffer';
+import KidCollection from './screens/pages/main/bestoffer/component/kidcollection/KidCollection';
+import WomenCollection from './screens/pages/main/bestoffer/component/womencollection/WomenCollection';
+import ManCollection from './screens/pages/main/bestoffer/component/mancollection/ManCollection';
+import SingleItem from './screens/pages/main/bestoffer/component/mancollection/components/singleItem/SingleItem';
+import Account from './screens/pages/main/account/Account';
+import Home from './screens/pages/main/home/Home';
+import CartBag from "./screens/pages/components/header/components/addtocart/CartBag"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainRoute />}>
-          <Route path="/" element={<Mainpage />} />
+          <Route path="/" element={<Home />} />
           <Route path="/track-order" element={<Trackorder />} />
-        </Route>   
+          <Route path="/Best-Offers" element={<Bestoffer />} />
+          <Route path="/Kid-Collection" element={<KidCollection />} />
+          <Route path="/Woman-Collection" element={<WomenCollection />} />
+          <Route path="/Man-Collection" element={<ManCollection />} />
+          <Route path="/Single-Item" element={<SingleItem />} />
+          <Route path="/Account" element={<Account />} /> 
+          <Route path="/Cart-bag" element={<CartBag />} />
+        
+        </Route>
+
         <Route path="/auth" element={<AuthRoute />}>
-          {/* <Route path="/auth/edit-profile" element={<EditProfile />} /> */}
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/Register" element={<Register />} />
-        </Route>        
+        </Route>
       </Routes>
     </BrowserRouter>
   )
